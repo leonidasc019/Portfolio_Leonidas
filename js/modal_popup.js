ba92b4a13b26ab1a6319bcf11100887f05e92cfa
Leônidas <leonidas19992011@hotmail.com>
2022-12-28 11:57:18 -0300

    Adicionando PopUp, Novo JS, novaparte da pagina

---
+++ js/modal_popup.js
@@ -0,0 +1,17 @@
+/*----------------------------------------------------*/
+/*	Modal Popup
+------------------------------------------------------*/
+$('.item-wrap a').magnificPopup({
+
+    type: 'inline',
+    fixedContentPos: false,
+    removalDelay: 300,
+    showCloseBtn: false,
+    mainClass: 'mfp-fade'
+
+});
+
+$(document).on('click', '.popup-modal-dismiss', function (e) {
+    e.preventDefault();
+    $.magnificPopup.close();
+});
