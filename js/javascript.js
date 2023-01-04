34c0dcfdf685ab759d0c6a67a71bcd75e3bb46da
Leônidas <leonidas19992011@hotmail.com>
2023-01-04 11:35:11 -0300

    Projeto Infinity

--- js/javascript.js
+++
@@ -1,9 +0,0 @@
-$(window).on('load', function () {
-    $('#preloader .inner');
-    $('#preloader').delay(950).fadeOut('slow');
-    $('body').delay(950).css({
-        'overflow': 'visible'
-    });
-})
-
-//------------- PRE LOADER EXIT---------------------
