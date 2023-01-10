ea62636f55db512282bc9594f7aa079102ae0436
Leônidas <leonidas19992011@hotmail.com>
2023-01-10 16:30:33 -0300

    MediaScreen Update

--- js/modal_popup.js
+++ js/modal_popup.js
@@ -3,9 +3,13 @@ const workImageBlock = document.querySelector('.image-wrap')
 const Work_Resume = document.querySelector('.Work_Resume')
 
 function openModal() {
-    modal.classList.add('active')
-    workImageBlock.classList.add('image_opac')
-    Work_Resume.classList.add('resume_opac')
+    if (window.innerWidth < 900) {
+        window.location.href = 'https://leonidas019portfolio.netlify.app/';
+    } else {
+        modal.classList.add('active')
+        workImageBlock.classList.add('image_opac')
+        Work_Resume.classList.add('resume_opac')
+    }
 }
 
 function closeModal() {
