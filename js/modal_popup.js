657e9e0f2b2b458d9a4021e5b6a64fa05602cb13
Leônidas <leonidas19992011@hotmail.com>
2023-02-14 18:21:00 -0300

    modificação e adição de novo projeto

--- js/modal_popup.js
+++ js/modal_popup.js
@@ -1,6 +1,9 @@
 const modal = document.querySelector('.modal')
-const workImageBlock = document.querySelector('.image-wrap')
+const modal2 = document.querySelector('.modal2')
+const workImageBlock2 = document.querySelector('.image-wrap')
 const Work_Resume = document.querySelector('.Work_Resume')
+const workImageBlock = document.querySelector('.image-wrap')
+
 
 function openModal() {
     if (window.innerWidth < 900) {
@@ -13,8 +16,26 @@ function openModal() {
     }
 }
 
+
+function openModal1() {
+    if (window.innerWidth < 900) {
+        window.location.href = 'https://projetocurso01.netlify.app';
+
+    } else {
+        modal2.classList.add('active')
+        workImageBlock2.classList.add('image_opac')
+        Work_Resume.classList.add('resume_opac')
+    }
+}
+
 function closeModal() {
     modal.classList.remove('active')
     workImageBlock.classList.remove('image_opac')
+    Work_Resume.classList.remove('resume_opac')
+}
+
+function closeModal() {
+    modal2.classList.remove('active')
+    workImageBlock2.classList.remove('image_opac')
     Work_Resume.classList.remove('resume_opac')
 }
